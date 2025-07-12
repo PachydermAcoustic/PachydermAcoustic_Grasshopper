29a1efba8805b2e22d0262765099a379636875f0
Arthur van der Harten <arthur.vanderharten@gmail.com>
2025-07-11 22:08:35 -0400

    2.6.0.21: - Improved stability for multi-layered materials that have been saved. - Added clustersources - a way of reducing complexity and memory usage for large collections of source objects. - Fixed an issue in Edge Diffraction - Mobile update of ECC libraries from Mendeley - Added background noise specification by RC, NC, and NR

---
+++ Env_NoiseRating.cs
@@ -0,0 +1,91 @@
+﻿//'Pachyderm-Acoustic: Geometrical Acoustics for Rhinoceros (GPL)   
+//' 
+//'This file is part of Pachyderm-Acoustic. 
+//' 
+//'Copyright (c) 2008-2025, Open Research in Acoustical Science and Education, Inc. - a 501(c)3 nonprofit 
+//'Pachyderm-Acoustic is free software; you can redistribute it and/or modify 
+//'it under the terms of the GNU General Public License as published 
+//'by the Free Software Foundation; either version 3 of the License, or 
+//'(at your option) any later version. 
+//'Pachyderm-Acoustic is distributed in the hope that it will be useful, 
+//'but WITHOUT ANY WARRANTY; without even the implied warranty of 
+//'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+//'GNU General Public License for more details. 
+//' 
+//'You should have received a copy of the GNU General Public 
+//'License along with Pachyderm-Acoustic; if not, write to the Free Software 
+//'Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. 
+
+using System;
+using Grasshopper.Kernel;
+using Pachyderm_Acoustic.Utilities;
+
+namespace PachydermGH
+{
+    public class NoiseRating : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MyComponent1 class.
+        /// </summary>
+        public NoiseRating()
+            : base("Noise Rating", "NR",
+                "Background noise curve specified in Noise Criteria",
+                "Acoustics", "Model")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddNumberParameter("Noise Rating", "NR", "The number of the specified Noise Rating spectrum", GH_ParamAccess.item);
+
+            Grasshopper.Kernel.Parameters.Param_Number param0 = (pManager[0] as Grasshopper.Kernel.Parameters.Param_Number);
+            if (param0 != null) param0.SetPersistentData(40);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddNumberParameter("Noise Spectrum", "NS", "The resulting spectrum from the NR curve specification...", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            double NS=40;
+            if (!DA.GetData<double>(0, ref NS)) return;
+
+            double[] NR_Curve = AcousticalMath.Noise_Rating(NS);
+
+            DA.SetDataList(0, NR_Curve);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                System.Drawing.Bitmap b = Properties.Resources.Geodesic_Source;
+                b.MakeTransparent(System.Drawing.Color.White);
+                return b;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("{6AD48AB2-08FF-4921-9B24-4F7DB16085AD}"); }
+        }
+    }
+}
---
+++ Env_RoomCriteria.cs
@@ -0,0 +1,91 @@
+﻿//'Pachyderm-Acoustic: Geometrical Acoustics for Rhinoceros (GPL)   
+//' 
+//'This file is part of Pachyderm-Acoustic. 
+//' 
+//'Copyright (c) 2008-2025, Open Research in Acoustical Science and Education, Inc. - a 501(c)3 nonprofit 
+//'Pachyderm-Acoustic is free software; you can redistribute it and/or modify 
+//'it under the terms of the GNU General Public License as published 
+//'by the Free Software Foundation; either version 3 of the License, or 
+//'(at your option) any later version. 
+//'Pachyderm-Acoustic is distributed in the hope that it will be useful, 
+//'but WITHOUT ANY WARRANTY; without even the implied warranty of 
+//'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+//'GNU General Public License for more details. 
+//' 
+//'You should have received a copy of the GNU General Public 
+//'License along with Pachyderm-Acoustic; if not, write to the Free Software 
+//'Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. 
+
+using System;
+using Grasshopper.Kernel;
+using Pachyderm_Acoustic.Utilities;
+
+namespace PachydermGH
+{
+    public class RoomCriteria : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MyComponent1 class.
+        /// </summary>
+        public RoomCriteria()
+            : base("Room Criteria", "RC",
+                "Background noise curve specified in Room Criteria",
+                "Acoustics", "Model")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddNumberParameter("Room Criteria", "RC", "The number of the specified Room Criteria spectrum", GH_ParamAccess.item);
+
+            Grasshopper.Kernel.Parameters.Param_Number param0 = (pManager[0] as Grasshopper.Kernel.Parameters.Param_Number);
+            if (param0 != null) param0.SetPersistentData(40);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddNumberParameter("Noise Spectrum", "NS", "The resulting spectrum from the RC curve specification...", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            double NS=40;
+            if (!DA.GetData<double>(0, ref NS)) return;
+
+            double[] RC_Curve = AcousticalMath.Room_Criteria(NS);
+
+            DA.SetDataList(0, RC_Curve);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                System.Drawing.Bitmap b = Properties.Resources.Geodesic_Source;
+                b.MakeTransparent(System.Drawing.Color.White);
+                return b;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("{D609D0BF-4237-4FA5-A5EE-46FB376D0B65}"); }
+        }
+    }
+}
